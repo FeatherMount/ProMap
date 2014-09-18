fbf6d67341354edc60cde61207c9ce89b0ac05c9
Zhou Zhou <zz2181@columbia.edu>
2014-09-17 22:50:34 -0400

    all journal names parsed

--- src/DataCollectingDriver.java
+++ src/DataCollectingDriver.java
@@ -1,9 +1,18 @@
+import au.com.bytecode.opencsv.CSVReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 public class DataCollectingDriver
 {
     public static void main(String[] args) throws Exception
     {
-        //String journalListFile = "../resource/NIH_PA_journal_list_March_2014.csv";
-        //CSVParser.parseCsvFile(journalListFile);
-        XMLDownloader.downloadXML("Nature", 1, 10);
+        String journalListFile = "../resource/NIH_PA_journal_list_March_2014.csv";
+        CSVReader reader = new CSVReader(new FileReader(journalListFile));
+        String [] nextLine;
+        ArrayList<String> journalNames = new ArrayList<String>();
+        while ((nextLine = reader.readNext()) != null)
+            journalNames.add(nextLine[0]);
+        for (String journalName : journalNames)
+            System.out.println(journalName);
+        //XMLDownloader.downloadXML("Nature", 1, 10);
     }
 }
--- src/HowToExecute.md
+++ src/HowToExecute.md
@@ -1,1 +1,1 @@
-java -classpath ./APIs/org.apache.httpcomponents.httpclient_4.3.5.jar:./APIs/commons-logging-1.2.jar:./ DataCollectingDriver
+java -classpath ./APIs/org.apache.httpcomponents.httpclient_4.3.5.jar:./APIs/commons-logging-1.2.jar:./:./APIs/opencsv-3.0.jar DataCollectingDriver
